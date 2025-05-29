1afd2e7f6080819d507a5e3e1e371a7457f9b792
Hafiz Muhammad Abdullah <hm.abdullah@2bvision.com>
2025-05-29 18:40:52 +0500

    add data some code

---
+++ screens/tabNavigation/components/CardSideBar.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const CardSideBar = () => {
+  return (
+    <View>
+      <Text>CardSideBar</Text>
+    </View>
+  )
+}
+
+export default CardSideBar
+
+const styles = StyleSheet.create({})
